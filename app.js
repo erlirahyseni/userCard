d428e3f47d496bbffcc1c72bd56a1b366c6fe15b
erlirahyseni <erlirahyseni2@gmail.com>
2022-09-19 16:34:19 +0200

    Project1

--- app.js
+++ app.js
@@ -13,10 +13,9 @@ form.addEventListener("submit", (e) => {
   let request = async () => {
     try {
       let res = await axios.get("https://swapi.dev/api/people/" + search);
-      let data = res.data; 
+      let data = res.data;
 
       document.querySelector(".list").innerHTML = `<b>Name:</b> ${data.name}<br> <b>Height:</b> ${data.height} <br> <b>Mass:</b> ${data.mass} <br> <b>Birth:</b> ${data.birth_year}`;  
-
   }
     catch (e) {
       console.log(e, "Error Found");
